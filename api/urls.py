15324b0b832cc0cf56acb6f3d9678ade6e9b1407
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-03-31 16:34:59 +0500

    transferred into class based view

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import get_all, get_by_id, create, upadate_by_id, get_all_completed, get_all_uncompleted, delete_by_id, complete_by_id
+from .views import Tasks, Tasks_id, Tasks_create, Complete, Completed, Uncompleted
 
 
 urlpatterns = [
-    path('tasks/', get_all),
-    path('tasks/<int:id>/', get_by_id),
-    path('tasks/create/', create),
-    path('tasks/<int:id>/update/', upadate_by_id),
-    path('tasks/<int:id>/delete/', delete_by_id),
-    path('tasks/<int:id>/complete/', complete_by_id),
-    path('tasks/completed/', get_all_completed),
-    path('tasks/uncompleted/', get_all_uncompleted),
+    path('tasks/', Tasks.as_view()),
+    path('tasks/<int:id>/', Tasks_id.as_view()),
+    path('tasks/create/', Tasks_create.as_view()),
+    path('tasks/<int:id>/update/', Tasks.as_view()),
+    path('tasks/<int:id>/delete/', Tasks.as_view()),
+    path('tasks/<int:id>/complete/', Complete.as_view()),
+    path('tasks/completed/', Completed.as_view()),
+    path('tasks/uncompleted/', Uncompleted.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,124 +1,128 @@
 from django.http import HttpRequest, JsonResponse
 from .models import Task
+import json
+from django.views import View
 
-def get_all(request: HttpRequest) -> JsonResponse:
-    '''get all tasks'''
-    tasks = Task.objects.all()
-    data = {'tasks': []}
+class Tasks(View):
 
-    for task in tasks:
-        data['tasks'].append({
+    def get(self, request: HttpRequest) -> JsonResponse:
+        '''get all tasks'''
+        tasks = Task.objects.all()
+        data = {'tasks': []}
+
+        for task in tasks:
+            data['tasks'].append({
+                'id': task.id,
+                'name': task.name,
+                'completed': task.completed,
+                'desciption': task.description,
+                'created': task.created,
+                'updated': task.updated,
+            })
+
+        return JsonResponse(data)
+
+    def delete(self, request: HttpRequest, id: int) -> JsonResponse:
+        task = Task.objects.get(pk=id)
+        task.delete()
+        return JsonResponse({})
+    
+    def post(self, request: HttpRequest, id: int) -> JsonResponse:
+        data = json.loads(request.body.decode("utf-8"))
+        task = Task.objects.get(pk=id)
+        task.name = data['name']
+        task.description = data['description']
+        task.completed = data['completed']
+        task.save()
+        data = {'task': {
             'id': task.id,
             'name': task.name,
             'completed': task.completed,
             'desciption': task.description,
             'created': task.created,
             'updated': task.updated,
-        })
+        }}
+        return JsonResponse(data)
 
-    return JsonResponse(data)
-
-def get_by_id(request: HttpRequest, id: int) -> JsonResponse:
-    task = Task.objects.get(pk=id)
-    data = {'task': {
-        'id': task.id,
-        'name': task.name,
-        'completed': task.completed,
-        'desciption': task.description,
-        'created': task.created,
-        'updated': task.updated,
-    }}
-    return JsonResponse(data)
-
-def create(request: HttpRequest) -> JsonResponse:
-    data = request.POST
-    task = Task.objects.create(
-        name=data['name'],
-        description=data['description'],
-        completed=data['completed'],
-    )
-    data = {'task': {
-        'id': task.id,
-        'name': task.name,
-        'completed': task.completed,
-        'desciption': task.description,
-        'created': task.created,
-        'updated': task.updated,
-    }}
-    return JsonResponse(data)
-
-def upadate_by_id(request: HttpRequest, id: int) -> JsonResponse:
-    data = request.POST
-    task = Task.objects.get(pk=id)
-    task.name = data['name']
-    task.description = data['description']
-    task.completed = data['completed']
-    task.save()
-    data = {'task': {
-        'id': task.id,
-        'name': task.name,
-        'completed': task.completed,
-        'desciption': task.description,
-        'created': task.created,
-        'updated': task.updated,
-    }}
-    return JsonResponse(data)
-
-def complete_by_id(request: HttpRequest, id: int) -> JsonResponse:
-    task = Task.objects.get(pk=id)
-    task.completed = True
-    task.save()
-    data = {'task': {
-        'id': task.id,
-        'name': task.name,
-        'completed': task.completed,
-        'desciption': task.description,
-        'created': task.created,
-        'updated': task.updated,
-    }}
-    return JsonResponse(data)
-
-def delete_by_id(request: HttpRequest, id: int) -> JsonResponse:
-    task = Task.objects.get(pk=id)
-    task.delete()
-    data = {'task': {
-        'id': task.id,
-        'name': task.name,
-        'completed': task.completed,
-        'desciption': task.description,
-        'created': task.created,
-        'updated': task.updated,
-    }}
-    return JsonResponse(data)
-
-def get_all_completed(request: HttpRequest) -> JsonResponse:
-    tasks = Task.objects.filter(completed=True)
-    data = {'tasks': []}
-
-    for task in tasks:
-        data['tasks'].append({
+class Tasks_id(View):
+    def get(self, request: HttpRequest, id: int) -> JsonResponse:
+        task = Task.objects.get(pk=id)
+        data = {'task': {
             'id': task.id,
             'name': task.name,
             'completed': task.completed,
             'desciption': task.description,
             'created': task.created,
             'updated': task.updated,
-        })
+        }}
+        return JsonResponse(data)
 
-    return JsonResponse(data)
+class Tasks_create(View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+        data = json.loads(request.body.decode("utf-8"))
+        task = Task.objects.create(
+            name=data['name'],
+            description=data['description'],
+            completed=data['completed'],
+        )
+        data = {'task': 
+            {
+                'id': task.id,
+                'name': task.name,
+                'completed': task.completed,
+                'desciption': task.description,
+                'created': task.created,
+                'updated': task.updated,
+            }
+        }
+        return JsonResponse(data)
 
-def get_all_uncompleted(request: HttpRequest) -> JsonResponse:
-    tasks = Task.objects.filter(completed=False)
-    data = {'tasks': []}
+class Complete(View):
+    def get(self, request: HttpRequest, id: int) -> JsonResponse:
+        if id is not None:
+            task = Task.objects.get(pk=id)
+            task.completed = True
+            task.save()
+            data = {'task': {
+                'id': task.id,
+                'name': task.name,
+                'completed': task.completed,
+                'desciption': task.description,
+                'created': task.created,
+                'updated': task.updated,
+            }}
+            return JsonResponse(data)
 
-    for task in tasks:
-        data['tasks'].append({
-            'id': task.id,
-            'name': task.name,
-            'completed': task.completed,
-            'desciption': task.description,
-            'created': task.created,
-            'updated': task.updated,
-        })
+class Completed(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        tasks = Task.objects.filter(completed=True)
+        data = {'tasks': []}
 
-    return JsonResponse(data)
+        for task in tasks:
+            data['tasks'].append({
+                'id': task.id,
+                'name': task.name,
+                'completed': task.completed,
+                'desciption': task.description,
+                'created': task.created,
+                'updated': task.updated,
+            })
+
+        return JsonResponse(data)
+
+class Uncompleted(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        tasks = Task.objects.filter(completed=False)
+        data = {'tasks': []}
+
+        for task in tasks:
+            data['tasks'].append({
+                'id': task.id,
+                'name': task.name,
+                'completed': task.completed,
+                'desciption': task.description,
+                'created': task.created,
+                'updated': task.updated,
+            })
+
+        return JsonResponse(data)
