daf25d287cfa9f29b4d294b36249559d6e6fca96
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-04-06 12:29:30 +0500

    added authorization

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import Tasks, Tasks_id, Tasks_create, Complete, Completed, Uncomplet
 
 urlpatterns = [
     path('user/', Registration.as_view()),
-    path('tasks/<int:id>/', Tasks.as_view()),
+    # path('tasks/<int:id>/', Tasks.as_view()),
     path('tasks/', Tasks.as_view()),
     path('tasks/create/', Tasks_create.as_view()),
     path('tasks/<int:id>/update/', Tasks.as_view()),
--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ import json
 from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from base64 import b64decode
 
 def to_dict(task):
     data = {
@@ -30,81 +32,120 @@ class Registration(View):
 
 class Tasks(View):
 
-    def get(self, request: HttpRequest, id = None) -> JsonResponse:
+    def get(self, request: HttpRequest) -> JsonResponse:
         '''get all tasks'''
-        if id == None:
-            tasks = Task.objects.all()
+        authorization = request.headers.get('Authorization').split()[1]
+
+        username, password = b64decode(authorization).decode().split(':')
+        # username, password = authorization['username'], authorization['password']
+
+        user = authenticate(username=username, password=password)
+        # print(username, password)
+        # print(user)
+
+        if user != None:
+            tasks = Task.objects.filter(user=user)
             data = {'tasks': []}
 
             for task in tasks:
                 data['tasks'].append(to_dict(task))
             return JsonResponse(data)
-        else:
-            try:
-                task = Task.objects.get(pk=id)
-                return JsonResponse(to_dict(task))
-            except ObjectDoesNotExist:
-                return JsonResponse({"status": "does not exist"})
+        return JsonResponse({"status": "Unauthorized"})
 
     def delete(self, request: HttpRequest, id: int) -> JsonResponse:
-        task = Task.objects.get(pk=id)
-        task.delete()
-        return JsonResponse({})
+        # authorizing user
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
+
+        if user != None:
+            task = Task.objects.filter(user=user, pk=id)
+            task.delete()
+            return JsonResponse({"status": "Item was deleted"})
+        return JsonResponse({"status": "Unauthorized"})
+
     
     def post(self, request: HttpRequest, id: int) -> JsonResponse:
-        data = json.loads(request.body.decode("utf-8"))
-        task = Task.objects.get(pk=id)
-        task.name = data['name']
-        task.description = data['description']
-        task.completed = data['completed']
-        task.save()
-        data = {'task': {
-            'id': task.id,
-            'name': task.name,
-            'completed': task.completed,
-            'desciption': task.description,
-            'created': task.created,
-            'updated': task.updated,
-        }}
-        return JsonResponse(data)
+        # authorizing user
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
 
-class Tasks_id(View):
-    def get(self, request: HttpRequest, id: int) -> JsonResponse:
-        task = Task.objects.get(pk=id)
-        data = {'task': {
-            'id': task.id,
-            'name': task.name,
-            'completed': task.completed,
-            'desciption': task.description,
-            'created': task.created,
-            'updated': task.updated,
-        }}
-        return JsonResponse(data)
-
-class Tasks_create(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        data = json.loads(request.body.decode("utf-8"))
-        task = Task.objects.create(
-            name=data['name'],
-            description=data['description'],
-            completed=data['completed'],
-        )
-        data = {'task': 
-            {
+        if user != None:
+            data = json.loads(request.body.decode("utf-8"))
+            task = Task.objects.filter(user=user, pk=id)
+            task.name = data['name']
+            task.description = data['description']
+            task.completed = data['completed']
+            task.save()
+            data = {'task': {
                 'id': task.id,
                 'name': task.name,
                 'completed': task.completed,
                 'desciption': task.description,
                 'created': task.created,
                 'updated': task.updated,
+            }}
+            return JsonResponse(data)
+        return JsonResponse({"status": "Unauthorized"})
+
+class Tasks_id(View):
+    def get(self, request: HttpRequest, id: int) -> JsonResponse:
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
+
+        if user != None:
+            task = Task.objects.get(user=user, pk=id)
+            if task != None:
+                data = {'task': {
+                    'id': task.id,
+                    'name': task.name,
+                    'completed': task.completed,
+                    'desciption': task.description,
+                    'created': task.created,
+                    'updated': task.updated,
+                }}
+                return JsonResponse(data)
+            else:
+                return JsonResponse({'status': 'Not found any tasks'})
+        return JsonResponse({"status": "Unauthorized"})
+
+class Tasks_create(View):
+    def post(self, request: HttpRequest) -> JsonResponse:
+
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
+
+        if user != None:
+            data = json.loads(request.body.decode("utf-8"))
+            task = Task.objects.create(
+                name=data['name'],
+                description=data['description'],
+                completed=data['completed'],
+            )
+            data = {'task': 
+                {
+                    'id': task.id,
+                    'name': task.name,
+                    'completed': task.completed,
+                    'desciption': task.description,
+                    'created': task.created,
+                    'updated': task.updated,
+                }
             }
-        }
-        return JsonResponse(data)
+            return JsonResponse(data)
+        return JsonResponse({"status" : "Unauthorized"})
 
 class Complete(View):
     def get(self, request: HttpRequest, id: int) -> JsonResponse:
-        if id is not None:
-            task = Task.objects.get(pk=id)
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
+
+        if user != None and id is not None:
+            task = Task.objects.filter(user=user, pk=id)
             task.completed = True
             task.save()
             data = {'task': {
@@ -116,37 +157,54 @@ class Complete(View):
                 'updated': task.updated,
             }}
             return JsonResponse(data)
+        if user is None:
+            return JsonResponse({"status" : "Unauthorized"})
+        else:
+            return JsonResponse({"status" : "Id is required"})
 
 class Completed(View):
     def get(self, request: HttpRequest) -> JsonResponse:
-        tasks = Task.objects.filter(completed=True)
-        data = {'tasks': []}
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
 
-        for task in tasks:
-            data['tasks'].append({
-                'id': task.id,
-                'name': task.name,
-                'completed': task.completed,
-                'desciption': task.description,
-                'created': task.created,
-                'updated': task.updated,
-            })
+        if user != None:
+            tasks = Task.objects.filter(user=user, completed=True)
+            data = {'tasks': []}
 
-        return JsonResponse(data)
+            for task in tasks:
+                data['tasks'].append({
+                    'id': task.id,
+                    'name': task.name,
+                    'completed': task.completed,
+                    'desciption': task.description,
+                    'created': task.created,
+                    'updated': task.updated,
+                })
+
+            return JsonResponse(data)
+        return JsonResponse({"status": "Unauthorized"})
 
 class Uncompleted(View):
     def get(self, request: HttpRequest) -> JsonResponse:
-        tasks = Task.objects.filter(completed=False)
-        data = {'tasks': []}
+        authorization = request.headers.get('Authorization').split()[1]
+        username, password = b64decode(authorization).decode().split(':')
+        user = authenticate(username=username, password=password)
 
-        for task in tasks:
-            data['tasks'].append({
-                'id': task.id,
-                'name': task.name,
-                'completed': task.completed,
-                'desciption': task.description,
-                'created': task.created,
-                'updated': task.updated,
-            })
+        if user != None:
+            tasks = Task.objects.filter(user=user, completed=False)
+            data = {'tasks': []}
 
-        return JsonResponse(data)
+            for task in tasks:
+                data['tasks'].append({
+                    'id': task.id,
+                    'name': task.name,
+                    'completed': task.completed,
+                    'desciption': task.description,
+                    'created': task.created,
+                    'updated': task.updated,
+                })
+
+            return JsonResponse(data)
+
+        return JsonResponse({'status': 'Unauthorized'})
